b19646df3a87ad23081cd35f5f28478ea7e857be
RenataTambani <renatatambani@gmail.com>
2022-03-11 17:13:54 -0300

    animacao-scroll

--- script.js
+++ script.js
@@ -61,3 +61,28 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade;
+        //console.log(sectionTop);
+        if (isSectionVisible < 0) {
+          //console.log("animar");
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
