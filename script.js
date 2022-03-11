80797ebcd41fd214fc895741a3ac01e7d9d134eb
RenataTambani <renatatambani@gmail.com>
2022-03-11 15:35:00 -0300

    faq-accordion-list

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((imgMenu, index) => {
+      imgMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
+}
 
-  tabMenu.forEach((imgMenu, index) => {
-    imgMenu.addEventListener("click", () => {
-      activeTab(index);
+initTabNav();
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion(event) {
+      this.classList.toggle("ativo");
+      console.log(
+        event.currentTarget.nextElementSibling.classList.toggle("ativo")
+      );
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
     });
-  });
+  }
 }
